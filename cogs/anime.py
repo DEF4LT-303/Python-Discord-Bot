1045ec80ad98d4c48d94d5751d967e20b2b3dd28
Ryan <ryanrafi72@gmail.com>
2021-12-22 10:04:32 +0000

    plez work

--- cogs/anime.py
+++ cogs/anime.py
@@ -27,7 +27,7 @@ class Anime(commands.Cog):
     embedVar.add_field(name='NSFW Status', value=anime.is_nsfw(), inline=True)
     embedVar.set_thumbnail(url=anime.poster)
 
-    await ctx.send(embed=embedVar)
+    await ctx.send(embed=embedVar) #test
 
 def setup(client):
 
